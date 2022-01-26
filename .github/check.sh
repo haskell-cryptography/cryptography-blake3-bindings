f2f59af6420f367a81257f0c412e6b5c57951c4d
Koz Ross <koz.ross@retro-freedom.nz>
2022-01-26 15:49:27 +1300

    Use hlint-setup and hlint-run instead of a manual build

--- .github/check.sh
+++ .github/check.sh
@@ -14,17 +14,6 @@ else
   exit 1
 fi
 
-if which "$1/hlint" > /dev/null ; then
-  for f in ${haskell_files}; do
-    if ! "$1/hlint" -h '.hlint.yaml' -q "$f" ; then
-      exit 1
-    fi
-  done
-else
-  echo "HLint not found, aborting."
-  exit 1
-fi
-
 if which "$1/cabal-fmt" > /dev/null ; then
   if ! "$1/cabal-fmt" -c "cryptography-blake3-bindings.cabal" ; then
     exit 1
